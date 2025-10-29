5dd62340e38209d2a9a6586e47f2d4989713f933
AlaaAbdullah244 <alaaabdullah173@gmail.com>
2025-10-29 20:16:12 +0300

    first two functios

---
+++ src/config/db.js
@@ -0,0 +1,22 @@
+// src/config/db.js
+import pg from "pg";
+import dotenv from "dotenv";
+
+dotenv.config(); // تحميل متغيرات البيئة من ملف .env
+
+const { Pool } = pg;
+
+// إنشاء اتصال بقاعدة البيانات
+export const pool = new Pool({
+  user: process.env.DB_USER,
+  host: process.env.DB_HOST,
+  database: process.env.DB_NAME,
+  password: process.env.DB_PASSWORD,
+  port: process.env.DB_PORT,
+});
+
+// اختبار الاتصال
+pool
+  .connect()
+  .then(() => console.log("✅ Connected to PostgreSQL database"))
+  .catch((err) => console.error("❌ Database connection error:", err));
